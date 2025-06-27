97911a22438064d62cc0da7f2dd73fbfda1c8889
Jose Gutierrez <josearmandog02@gmail.com>
2025-06-27 09:57:28 -0400

    reduce classes added

---
+++ server/05-reduce.js
@@ -0,0 +1,12 @@
+const totals = [1,2,3,4]
+
+let sum = 0
+for (let i = 0; i < totals.length; i++) {
+  const element = totals[i];
+  sum += element
+}
+
+console.log(sum)
+
+const rta = totals.reduce((sum, element) => sum += element, 0)
+console.log('rta', rta)
